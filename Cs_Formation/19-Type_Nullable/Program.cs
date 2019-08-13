4c37d3e70e635e4cf374800b07e3fbd66034df22
jeremie <jeremie.pichot@gmail.com>
2019-08-13 09:09:36 +0200

    Type générique

--- Cs_Formation/19-Type_Nullable/Program.cs
+++ Cs_Formation/19-Type_Nullable/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace _19_Type_Nullable
 {
@@ -9,9 +11,26 @@ namespace _19_Type_Nullable
             Console.WriteLine("Les Type generiques:");
             Console.WriteLine("--------------------");
 
+
+            Queue<int> fileAttente = new Queue<int>();
+            fileAttente.Enqueue(3);
+            fileAttente.Enqueue(1);
+            int valeur = fileAttente.Dequeue();
+            valeur = fileAttente.Dequeue();
+
+            Dictionary<string, Personne> annuaire = new Dictionary<string, Personne>();
+            annuaire.Add("06 61 67 93 75", new Personne { Prenom = "Jeremie" });
+            annuaire.Add("03 23 23 23 23", new Personne { Prenom = "Antoine"});
+            Personne p = annuaire["06 61 67 93 75"]; // Ici p contient la propriete Prenom valant Jeremie
+
+            Afficheur(3);
+
+
         }
+
         // le Type Nullable est definie avec le "?"
-        /*
+
+        /* * *
          * 2 methode predefinie pour les utilisers
          * 
          *       HasValue retourne s'il y a une valeur ou non
@@ -20,13 +39,37 @@ namespace _19_Type_Nullable
          *       
          *    Si HasValue return False alors Value est Null
          */
-        int? ii = 42;
-        if (x.HasValue)
+
+        /* * * 
+         * 
+         * int? ii = 42;
+         * 
+         * if (x.HasValue)
+         *  {System.Console.WriteLine(ii.Value);}
+         * else
+         *  {System.Console.WriteLine("Pas de valeur defiie");}
+         * 
+         * 
+         */
+
+
+        // les classes queue<> permettent de gérer une fille d'attente style FIFO (= Premier entrer premier sorti )
+
+
+
+    }
+
+    internal class Personne
+    {
+    }
+
+    public class Afficheur
+    {
+        public class void Affiche( XObject o)
         {
-        System.Console.WriteLine(ii.Value);
+            Console.WriteLine("Afficheur d'objet :");
+            Console.WriteLine("\tType : " + o.GetType());
+            Console.WriteLine("\tRepresentation : " + o.ToString());
         }
-        else
-        {
-        System.Console.WriteLine("Pas de valeur defiie");
-        }
+    }
 }
